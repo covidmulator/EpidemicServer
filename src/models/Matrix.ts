2d6dc467855bd547b09a183e9621f06e5cfff37e
gtg7784 <tae.gun7784@gmail.com>
2020-10-25 13:04:18 +0900

    update

--- src/models/Matrix.ts
+++ src/models/Matrix.ts
@@ -2,12 +2,13 @@ import mongoose, { Schema } from 'mongoose'
 
 export interface MatrixModel {
   _id: string;
-
+  type: string;
   matrix: Array<Array<number>>;
 }
 
 const MatrixSchema: Schema<MatrixModel> = new Schema(
   {
+    type: { type: String; required: true },
     matrix: [[{ type: Number, required: true }]]
   },
   { timestamp: true }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,10 +7,26 @@ router.post(
   '/matrix', 
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const { matrix: _matrix, type } = req.body;
       const matrix = await new Matrix({
-        matrix: req.body.matrix
+        matrix: _matrix,
+        type: type
       }).save()
       res.status(200).json({ matrix: matrix })
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.get(
+  '/matrix',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+      const matrix = await Matrix.find({ type: type })
+
+      res.status(200).json({ data: matrix });
     } catch (err) {
       next(err)
     }
